8f3501aa76f1dba4891ab61997eae7a43042ba68
gkskiran07-cmd <gkskiran07@gmail.com>
2026-06-20 05:52:26 +0000

    checkout

---
+++ databricks_code.py
@@ -0,0 +1,18 @@
+data4 = [
+    (1, "raj"),
+    (2, "ravi"),
+    (3, "sai"),
+    (5, "rani")
+]
+
+cust = spark.createDataFrame(data4, ["id", "name"])
+cust.show()
+
+data3 = [
+    (1, "mouse"),
+    (3, "mobile"),
+    (7, "laptop")
+]
+
+prod = spark.createDataFrame(data3, ["id", "product"])
+prod.show()
